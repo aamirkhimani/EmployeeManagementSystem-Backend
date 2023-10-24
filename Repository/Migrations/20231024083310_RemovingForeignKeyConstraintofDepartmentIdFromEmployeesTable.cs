7013098eb4ed0b10d59550cde2d2dee75c1019d6
aamiraminali <aamir.amin@logicielservice.com>
2023-10-24 10:45:51 +0200

    Removing ForeignKetConstraint from Employees table of departmentId

---
+++ Repository/Migrations/20231024083310_RemovingForeignKeyConstraintofDepartmentIdFromEmployeesTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingForeignKeyConstraintofDepartmentIdFromEmployeesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
