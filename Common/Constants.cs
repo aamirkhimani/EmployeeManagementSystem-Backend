8df093e9475d1798ed28eda6bd7cf533555060a6
aamiraminali <aamir.amin@logicielservice.com>
2023-12-31 15:26:04 +0100

    Implementing FluentValidation on Registration Action

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -7,8 +7,11 @@ namespace Common
 		{
 		}
 
-		public static readonly string UsernameRegex = "^(?:(?!\\s)[\\w.+-]+@(?:\\w+\\.)+[a-zA-Z]{2,}|[\\w.+-]+)$";
+		public static readonly string UsernameRegex = "^[0-9A-Za-z]{6,16}$";
 		public static readonly string PasswordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
-	}
+		public static readonly string NameRegex = "^[a-zA-Z]+([ \\-']{0,1}[a-zA-Z]+){0,2}[.]{0,1}$";
+		public static readonly string EmailRegex = "(?:[a-z0-9!#$%&'*+\\=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+    }
 }
 
---
+++ EMS-Backend/Validators/RegistrationValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Common;
+using Common.Models.Request;
+using FluentValidation;
+
+namespace EMS_Backend.Validators
+{
+	public class RegistrationValidator: AbstractValidator<RegistrationRequest>
+	{
+		public RegistrationValidator()
+		{
+			RuleFor(registrationRequest => registrationRequest.FirstName).NotNull()
+				.NotEmpty()
+				.Matches(Constants.NameRegex);
+
+			RuleFor(registrationRequest => registrationRequest.LastName).NotNull()
+				.NotEmpty()
+				.Matches(Constants.NameRegex);
+
+			RuleFor(registrationRequest => registrationRequest.Email).NotNull()
+				.NotEmpty()
+				.Matches(Constants.EmailRegex);
+
+			RuleFor(registrationRequest => registrationRequest.Username).NotNull()
+				.NotEmpty()
+				.Matches(Constants.UsernameRegex);
+
+			RuleFor(registrationRequest => registrationRequest.Password).NotNull()
+				.NotEmpty()
+				.Matches(Constants.PasswordRegex);
+		}
+
+		
+	}
+}
+
